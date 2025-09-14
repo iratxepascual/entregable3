a2e0c4f5b983d5f88e04774866e0c916ddb61d8f
Iratxe Pascual <iratxe.pascual@xarxatecactiva.com>
2025-09-14 11:38:09 +0200

    cd y disco duro creados

---
+++ src/CD.ts
@@ -0,0 +1,21 @@
+import { AlmacenamientoFisico } from "./AlmacenamientoFisico.js";
+
+export class CD extends AlmacenamientoFisico{
+
+    girar(): void {
+        console.log("CD girando en el lector");
+    }
+
+    leerDatos(): void {
+        console.log("Leyendo datos del CD");
+    }
+
+    escribirDatos(): void {
+        console.log("Grabando datos en el CD");
+    }
+
+    reportarInformacion(): void {
+        console.log(`Nombre: ${this.nombre}\nCapacidad: ${this.capacidad}\nContenido: ${this.contenido}\nTipo de almacenamiento: ${this.tipoAlmacenamiento}.`);
+    }
+
+}
---
+++ src/DiscoDuro.ts
@@ -0,0 +1,21 @@
+import { AlmacenamientoFisico } from "./AlmacenamientoFisico.js";
+
+export class DiscoDuro extends AlmacenamientoFisico{
+
+    girar(): void {
+        console.log("Disco duro girando");
+    }
+
+    leerDatos(): void {
+        console.log("Leyendo datos del disco duro");
+    }
+
+    escribirDatos(): void {
+        console.log("Grabando datos en el disco duro");
+    }
+
+    reportarInformacion(): void {
+        console.log(`Nombre: ${this.nombre}\nCapacidad: ${this.capacidad}\nContenido: ${this.contenido}\nTipo de almacenamiento: ${this.tipoAlmacenamiento}.`);
+    }
+
+}
